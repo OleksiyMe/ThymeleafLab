65ecf9fbf1a7b135803928b5e7d49089477eab91
Oleksiy Melnychuk <melnichuk@gmail.com>
2022-10-06 19:22:08 -0400

    All almost done

--- src/main/java/com/cydeo/spring05thymeleaf/controller/GeneralController.java
+++ src/main/java/com/cydeo/spring05thymeleaf/controller/GeneralController.java
@@ -19,6 +19,7 @@ public class GeneralController {
 
     private final ProductService productService;
     private final CartService cartService;
+
     public GeneralController(ProductService productService, CartService cartService) {
         this.productService = productService;
         this.cartService = cartService;
@@ -46,24 +47,32 @@ public class GeneralController {
     }
 
     @GetMapping("/show-cart/{id}/{quantity}")
-    public String addProductToCart(@PathVariable("id") UUID uuid, @PathVariable("quantity") Integer quantity,
-                                   Model model){
-        cartService.addToCart(uuid, quantity);
+    public String addProductToCart(@PathVariable("id") String id,
+                                   @PathVariable("quantity") Integer quantity,    Model model) {
+        cartService.addToCart(UUID.fromString(id), quantity);
         model.addAttribute("cart", CartServiceImpl.CART);
         model.addAttribute("cartItems", CartServiceImpl.CART.getCartItemList());
 
 
-      return "/cart/show-cart";
+        return "/cart/show-cart";
     }
 
-   /* @GetMapping("/show-cart")
-    public String addProductToCart(Model model){
+    @GetMapping("/show-cart")
+    public String addProductToCart( Model model) {
 
+        model.addAttribute("cart", CartServiceImpl.CART);
+        model.addAttribute("cartItems", CartServiceImpl.CART.getCartItemList());
 
 
         return "/cart/show-cart";
     }
-*/
+    @GetMapping("/delete-from-cart/{id}")
+    public String addProductToCart(@PathVariable("id") String id) {
+
+        cartService.deleteFromCart(UUID.fromString(id));
+
+        return "redirect:/show-cart";
+    }
 
 
 }
